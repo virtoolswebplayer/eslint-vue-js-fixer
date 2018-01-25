2b87b0060eb168dfc98d9010c680e0aa0c230e37
高乐天 <gaoletian@puhuifinance.com>
2018-01-26 04:29:04 +0800

    更新依赖包

--- test/a.vue
+++ test/a.vue
@@ -1,14 +1,39 @@
-<template name="23234234234">
-  <div :onreset="null">
+<template>
+<div>
+</div>
+<div>
     <!--sdflks -->
-    <item name="gaoletian"/>
-    <item name="gaoletian"/>
-    <item name="gaoletian"/>
-  </div>
+    <item name="gaoletian" />
+    <item name="gaoletian" />
+    <item name="gaoletian" />
+
+    <tab-bar>
+        <tab name="one" />
+        <tab name="two" />
+        <tab name="three" />
+    </tab-bar>
+
+    <div>
+        <item>
+            <img src="" alt="">
+        </item>
+    </div>
+</div>
 </template>
 
 <!--sdflks -->
-<style lang="less" rel="stylesheet/less" scoped>
+<script lang="babel" type="text/babel">
+export default {
+  props: {},
+  data() {
+    return { msg: 'hello world' };
+  },
+  methods: {}
+};
+</script>
+
+<!--sdflks -->
+<style lang="less" rel="stylesheet/less">
   @import "../utils/_vars.less";
   @import "../utils/_mixins.less";
 
@@ -16,14 +41,3 @@
     left: 10em;
   }
 </style>
-<script lang="babel" type="text/babel">
-  export default {
-    props: {},
-    data() {
-      return {
-        msg: 'hello world',
-      };
-    },
-    methods: {},
-  };
-</script>
